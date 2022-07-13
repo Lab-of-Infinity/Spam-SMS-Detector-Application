7a468cd13ac8d6c9f9d7e826fb655062419bf724
Lokesh Baviskar <90597433+Lab-of-Infinity@users.noreply.github.com>
2022-07-13 16:53:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pickle
 import string
 # from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
+import nltk
 from PIL import Image
 
 ps = PorterStemmer()
